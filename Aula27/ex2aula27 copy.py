23654ddb2fd75a6b53756a423e612d91ecb86bc9
Gabriel Correia <gabrielmal38@gmail.com>
2019-12-17 11:50:33 -0300

    aula 28

--- Aula27/ex2aula27 copy.py
+++ Aula27/ex2aula27 copy.py
@@ -1,5 +1,6 @@
 # Aula 21 - 16-12-2019
 #Operadores in is ==
+#is
 
 from geradorlista import lista_simples_int_str
 from geradorlista import lista_simples_int
@@ -57,22 +58,79 @@ print(f'O maior número da lista 2 é: {max(lista[1])}')
 
 # 4) Há o número 10 dentro da lista[0]?
 
+# def encontrar(parametro):
+#     lista_vazia = []
+#     for i in range(len(lista[0])):
+#         for (j) in (i):
+#             if buscar in lista[i][j]:
+#                 lista_vazia.append(i,j)
+
+# en  = encontrar(10)
+
+# print(en)
+
+lista[0].append(10)
+
+if 10 in lista[0]:
+    print(f'Sim há o número 10 em lista[0]')
+else:
+    print('Não, tem o número 10 em lista[0]')
+
+print(lista[0])
+
+
+
 
 # 5) Há o número 20 dentro da lista[1]?
+if 20 in lista[1]:
+    print(f'Sim o número 20 está em lista[1]')
+else:
+    print(f'Não,tem o número 20 em lista[1] ')
+
+print(lista[1])
 
 
 # 6) Quantos números da lista[0] tem dentro da lista[1]?
 
+contador = 0
+for i in lista[0]:
+    if i in lista[1]:
+        contador = contador + 1
+
+print(contador) 
+print(lista[0])
+print(lista[1])
+        
+    
+
+    
 
 # 7) Mostre os números da lista[0] que estão dentro da lista[1]
 
+contador2 = 0
+for i in lista[0]:
+    for j in lista[1]:
+        if i == j :
+            contador2 += 1 
+else:
+    print('Não a numeros iguais')
+
+print(f'lista zero:{lista[0]}','\n')
+print(f'lista um:{lista[1]}','\n')
+print(f'total:{contador2}','\n')
+
 
 # 8) Mutliplique a soma da lista[0] com cada item da lista[1]
+somar = sum(lista[0])
+for i in lista[1]:
+    print(f'{somar * i}')
 
 
 # 9) Faça uma divizão inteira do maior número da lista pelo menor numero da lista. Após verifique 
 # se o resultado está dentro de uma das listas, se sim, diga qual!
-
-
+numero_maior = max(lista[1])
+numero_menor  = min(lista[0])
+divisao_inteira = (numero_maior / numero_menor)
+print(divisao_inteira)
 # 10) Ferifique se o maior número da lista[0] está dentro da lista[1] e se o menor número da
 # lista[1] está na lista[0]
