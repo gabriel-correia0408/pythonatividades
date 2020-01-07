be66588fb6f44ae446d8db974c43e3d84194c0ca
gabriel-correia0408 <57449564+gabriel-correia0408@users.noreply.github.com>
2020-01-07 12:06:18 -0200

    Aula29
    exercicio 01 HB, resolvido.

--- Aula29/ex1hb.py
+++ Aula29/ex1hb.py
@@ -7,76 +7,72 @@
 # serviço de voo podem dirigir este veículo. É também política da empresa que nenhum dos oficiais
 # pode ficar sozinho com o chefe de serviço de bordo, e nem nenhuma das comissárias pode ficar
 # sozinha com o piloto.  No terminal de embarque estão os seis tripulantes e ainda um policial 
-# junto com um presidiário. Estes oito elementos terão que embarcar segundo ás regras descritas acima. 
+# junto com um presidiário. Estes oito elementos terão que embarcar segundo as regras descritas acima. 
 # A empresa não coloca nenhum limite para o número de viagens entre o terminal e o avião.
 # Por motivos de segurança o presidiário não pode ficar sozinho em momento algum com os
 # tripulantes sem a presença do policial, nem no terminal e nem no avião. De forma a facilitar o
 # processo, a empresa autorizou que o policial pudesse dirigir o veículo também.
-
 # Requisitos:
 # 1 - Sempre que o Fortwo se mover, apresentar no console quando ele chega no destino
 # 2 - Sempre que acontecer um embarque, apresentar quais elementos estão embarcando
 # 3 - Sempre que acontecer um embarque no terminal, apresentar quem está no terminal
 # 4 - Sempre que acontecer um embarque no avião, apresentar quem está no avião
 # 5 - Deve ser feito em Python
-
-aviao  = []
-terminal = ['Piloto','oficial1','Oficial2','Chefe','Comissaria1','Comissaria2','Policial','Presidiário']
-for a in range(1):
-    print('\nSMARTWO saiu para buscar dois passageiros: ')
-    aviao.append(terminal[0])
-    aviao.append(terminal[1])
-    terminal.pop(0)
-    terminal.pop(0)
-    print(f'\nEstão juntos no avião o {aviao[0]} e {aviao[1]}')
-    print(f'\nO Smartwo saiu para buscar o passageiro nele está o motorista: {aviao[0]}, ele volta e fica no terminal')
-    print(f'\nAtualamente estão no terminal {terminal}')
-    # aviao = Piloto oficial1               oficial1[0] comissaria1 [1] 
-    # Oficial2', 'Chefe', 'Comissaria1', 'Comissaria2', 'Policial', 'Presidiário'
-    terminal.append(aviao[0])
-    aviao.pop(0)
-    aviao.append(terminal[1])
-    aviao.append(terminal[2])
-    terminal.pop(1)
-    terminal.pop(1)
-    #'oficial1', 'Chefe', 'Comissaria1']
-    # 'Oficial2', 'Comissaria2', 'Policial', 'Presidiário', 'Piloto']
-    print(f'\nNa ultima viagem chegaram ao avião: {aviao[1]} e {aviao[2]}')
-    print(f'\nO Smartwo saiu para buscar um passageiro nele está o motorista: {aviao[1]}')
-    print(f'\nAtualamente estão no terminal {terminal}')
-    terminal.append(aviao[1])
-    aviao.pop(1)
-    aviao.append(terminal[4])
-    aviao.append(terminal[5])
-    terminal.pop(4)
-    terminal.pop(4)
-    
-    print(f'\nE estão no momento dentro do avião {aviao}')
-    print(f'\nE estão no momento no terminal {terminal}')
-    # #aviao = oficial[0],comissaria[1], piloto[2],chefe[3]
-    terminal.append(aviao[2])
-    aviao.pop(2)
-    aviao.append(terminal[1])
-    aviao.append(terminal[4])
-    terminal.append(aviao[2])
-    aviao.pop(2)
-    terminal.pop(4)
-    terminal.pop(1)
-    # terminal.append()
-    print(f'\nE estão no momento dentro do avião {aviao}')
-    print(f'\nE estão no momento no terminal {terminal}')
-    aviao.append(terminal[1])
-    aviao.append(terminal[2])
-    terminal.pop(1)
-    terminal.pop(2)
-    print(f'Estão no momento no avião {aviao}')
-    print(f'\nE estão no momento no terminal {terminal}')
-
-
-
-
-# print(aviao)
-# print(terminal)
-
-
-        
+from time import sleep
+print('#'*10, 'HBSIS AIRLINES', '#'*10)
+aviao = []
+terminal = ['Piloto','Oficial 1','Oficial 2','Chefe de Voo','Comissaria 1','Comissaria 2','Policial','Presidiario']
+def status():
+    sleep(2)
+    print(f'\nSobraram no terminal os seguintes passageiros: {terminal}')
+    sleep(2)
+    print(f'\nNo avião estão o seguintes passageiros: {aviao}')
+    sleep(2)
+def embarque_e(policial,piloto):
+    print(f'\nEmbarcaram no ForTwo o {policial} e o {piloto}.')
+    aviao.append(piloto)
+    aviao.append(policial)
+    terminal.remove(policial)
+    terminal.remove(piloto)
+def embarque_for_two(mot, pas):
+    print(f'\nEmbarcaram no ForTwo o {mot} e o {pas}.')
+    terminal.remove(mot)
+    terminal.remove(pas)
+    aviao.append(pas)
+    print(f'\nO {mot} volta para o terminal para a proxima viagem.')
+    terminal.append(mot)
+def adic_terminal(passageiro):
+    terminal.append(passageiro)
+    aviao.remove(passageiro)
+    print(f'\nO {passageiro} volta para o terminal para a proxima viagem.')
+def adic_aviao(passageiro1):
+    terminal.remove(passageiro1)
+    aviao.append(passageiro1)
+def solucao_exercicio():
+    sleep(2)
+    print('\nO automovel partiu ao destino para buscar passageiros.')
+    sleep(2)
+    #========== viagem 1
+    embarque_for_two('Piloto','Oficial 1')
+    status()
+    #========== viagem 2
+    embarque_for_two('Chefe de Voo','Comissaria 1')
+    status()
+    #========== viagem 3
+    embarque_for_two('Piloto','Oficial 2')
+    status()
+    #========== viagem 4
+    embarque_for_two('Chefe de Voo','Piloto')
+    status()
+    #========== viagem 5
+    embarque_for_two('Chefe de Voo','Comissaria 2')
+    status()
+    #========== viagem 6
+    embarque_e('Policial','Presidiario')
+    status()
+    adic_terminal('Piloto')
+    #========== viagem 7
+    embarque_for_two('Piloto','Chefe de Voo')
+    adic_aviao('Piloto')
+    status()
+solucao_exercicio()
