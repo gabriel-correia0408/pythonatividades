8b1c2e1fd02f3472887eb927f847145e3693c020
gabriel-correia0408 <57449564+gabriel-correia0408@users.noreply.github.com>
2020-01-09 16:57:45 -0300

    Aula extra exercicio

---
+++ Aulas_extras/exercicios_maykon.py
@@ -0,0 +1,19 @@
+pessoa    = ['Carol','Thiago','Matheus']
+lista_db  = ['sql', 'postgree', 'mongodb']
+lista_lp  = ['php','java','python']
+lista_frw = ['react', 'vue', 'angular']
+
+tupla_dados= ('l_backend','banco_dados','fremework')
+
+lista_dicio = []
+
+
+
+for i in range(1):
+    dicio = {f'{tupla_dados[0]}:{pessoa[0]}'}
+    lista_dicio.append(dicio)
+
+
+print(lista_dicio)
+
+
