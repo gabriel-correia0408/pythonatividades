fa0573777b10be8b8c085cdd564aa84f2c923cfa
gabriel-correia0408 <57449564+gabriel-correia0408@users.noreply.github.com>
2020-01-09 17:07:38 -0300

    Aula a tarde python

--- Aulas_extras/exercicios_maykon.py
+++ Aulas_extras/exercicios_maykon.py
@@ -3,14 +3,14 @@ lista_db  = ['sql', 'postgree', 'mongodb']
 lista_lp  = ['php','java','python']
 lista_frw = ['react', 'vue', 'angular']
 
-tupla_dados= ('l_backend','banco_dados','fremework')
+tupla_dados= ('nome','banco_dados','l_backend','fremework')
 
 lista_dicio = []
 
 
 
 for i in range(1):
-    dicio = {f'{tupla_dados[0]}:{pessoa[0]}'}
+    dicio = {tupla_dados[0]:, tupla_dados[1]:lista_lp}
     lista_dicio.append(dicio)
 
 
