48f35c2ad15e2b506e83aee5de8c67c9d4a275fa
GABRIEL <gabrielmal38@gmail.com>
2020-02-03 12:21:56 -0300

    Aula42

--- Aula41/dao/arquivo_dao.py
+++ Aula41/dao/arquivo_dao.py
@@ -6,13 +6,11 @@ class metodos_dao:
     def __init__(self):
         self.connection = MySQLdb.connect(hodt='')
 
-    def list_all(self)
+    def list_all(self):
         return: 'Listando todos '
 
     def get_by_id(self,id):
         return 'Listando pessoa de id: {}'.format()
-
-
 
     def get(self):
         msg =  self.dao.list_all()
