b6d023d3f8ca0023bdf8e004b468b1a72e54dd0e
GABRIEL <gabrielmal38@gmail.com>
2020-02-04 11:41:59 -0300

    "AULA42"

--- Aula42/dao/estado_dao.py
+++ Aula42/dao/estado_dao.py
@@ -1,11 +1,11 @@
-import MySQLdb
+import MySQLdb #importando o banco de dados "MySQLdb"
 
-from Aula42.estado_model import EstadoModel
+from Aula42.estado_model import EstadoModel#importanta da pasta "Aula42" e do arqivo"estado_model" a classe"EstadoModel"
 
-class EstadoDao:
-    def __init__(self):
-        self.connection = MySQLdb.connect(host='mysql.topskills.dev', database='topskills01', user='topskills01', passwd='ts2019' )
-        self.cursor =  self.connection.cursor()
+class EstadoDao:#Criando a classe dao aonde ira conter os métodos
+    def __init__(self):#iniciando com  o construtar (__init__) ele é usado para inicializar o objeto para poder se criado a instancia
+        self.connection = MySQLdb.connect(host='mysql.topskills.dev', database='topskills01', user='topskills01', passwd='ts2019' )#criando a conexao com bd
+        self.cursor =  self.connection.cursor()#definindo o cursor da conexao
 
     def listar_todos(self): # Criando método para listar todos da lista,do banco de dados
         self.cursor.execute("SELECT * FROM topskills01.ESTADOS")
@@ -17,5 +17,30 @@ class EstadoDao:
             #os valores que estão contidos em "estados"
         return lista_estados # retornando para  a lista "lista_estados"
 
-    def buscar_por_id(self, id):
-        self.cursor.execute("SELECT * FROM topskills01.ESTADOS WHERE  ")
+    def buscar_por_id(self, id):#criando método "buscar_por_id", o parametro "self" faz a ligação, o id vem do arquivo estado_model
+        self.cursor.execute("SELECT * FROM topskills01.ESTADOS WHERE ID = {}".format(id))#passando o "self.cursor"para fazer a conexao" eo "execute"para executar o comando anterior
+        estado = self.cursor.fetchone()#criando uma variavél "estado" que equivale ao "self.cursor" que fera o fetchone=buscar um
+        e = EstadoModel(estado[1], estado[2], estado[3], estado[0])#criando a variavél "e" para poder receber,os valores do EstadoModel, e buscar eles por posição
+        return e.__dict__#um método do python que transfroma o valor em dicionário
+
+    def inserir(self):#criando método inserir.
+        self.cursor.execute("""INSERT INTO topskills01.ESTADOS    
+                            (regiao, estado, populacao)
+                            VALUES('{}', '{}', '{}')""".format(estado.regiao, estado.estado, estado.populacao))#pagando a varivél "estado"
+                            #que contém os dados do bd, e adicioando ,através do comando INSERT... por parametro por ex:regiao populacao
+
+    def atualizar(self, estado: EstadoModel):#Criando método para atualizar,passando o "EstadoModel" para trazer os parametros para se completar o código
+        self.cursor.execute(""" UPDATE topskill01.ESTADOS 
+                         SET
+                             regiao
+                             estado
+                            populacao
+                        WHERE ID = {}""".format(pessoa.nome, pessoa.sobrenome, pessoa.idade, pessoa.id))
+        self.connection.commit()#passnado a conexao e salvando a atualização através commit
+        return estado.__dict__ #passavdo os dados para dicionário através do "__dict__"
+
+    def delete(self):#criando méto delete para apagar
+        self.cursor.execute(""" DELETE FROM topskills01.ESTADOS WHERE ID = {}""".format(id) #passando comando do que e quero apagar
+
+
+                                )
--- Aula42/model/estado_model.py
+++ Aula42/model/estado_model.py
@@ -1,5 +1,5 @@
-class EstadoModel:
-    def __init__(self, regiao, estado, populacao,id=0):
+class EstadoModel:#criando a classe EstadoModel
+    def __init__(self, regiao, estado, populacao,id=0):#o "id" vai por ultimo ,pois ele tem uma valor pré-definido
         self.id = id
         self.regiao = regiao
         self.estado = estado
