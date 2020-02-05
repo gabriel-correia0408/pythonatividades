9828079e25bccb27b808f53b928f82432541a869
GABRIEL <gabrielmal38@gmail.com>
2020-02-05 11:19:20 -0300

    Aula44

--- Aula42/controller/estado_controller.py
+++ Aula42/controller/estado_controller.py
@@ -1,1 +1,29 @@
-from flask_restful import
+# o resource serve para podermos fazer as quatro requisições http get put post delete
+from flask_restful import Resource
+# o request serve para fazer a requisão do json,para o código.
+from Flask import  request
+
+
+
+# importando da minha pasta "Aula42", da sub-pasta "dao", e do arquivo "estado_dao", a classe "EstadoDao"
+from Aula42.dao.estado_dao import EstadoDao
+# importando da minha pasta "Aula42", da sub-pasta "model", e do arquivo "estado_model", a classe "EstadoModel"
+from Aula42.model.estado_model import EstadoModel
+
+
+# Criando a classe "EstadoController" passando o parametro resource para poder fazer as 4 rewuisições http
+class EstadoController(Resource):
+    # criando um método ,passando o construtor __init__,que permite que a classe inicialize os atributos da classe
+    def __init__(self):
+        # criando uma varivél "dao" que recebera ou melhor será minha classse "EstadoDao"
+        self.dao = EstadoDao()
+
+
+    # é passado o valor "none"para quando for testado e caso tenha um valor none ele nã entra no if
+    def get(self, id=None):
+        # o if faz a pergunta de que se tiver "id" ou melhor um valor no id, ele entra na linha a baixo ,caso contrário não
+        if id:
+            # o reutrn vai mandar o "id" para  para a variavél dao que contém a classe EstadoDao,e ira aplicano no métdo "buscar_por_id"
+            return self.dao.buscar_por_id()
+        # caso como citado nas linha acima ,ele não entre no if, ele entra n alinha abaixo,que reotrnara o método listar_todos
+        return self.dao.listar_todos()
--- Aula42/dao/estado_dao.py
+++ Aula42/dao/estado_dao.py
@@ -1,46 +1,63 @@
 import MySQLdb #importando o banco de dados "MySQLdb"
 
 from Aula42.estado_model import EstadoModel#importanta da pasta "Aula42" e do arqivo"estado_model" a classe"EstadoModel"
+#Criando a classe dao aonde ira conter os métodos
+class EstadoDao:
+    # iniciando com  o construtar (__init__) ele é usado para inicializar o objeto para poder se criado a instancia
+    def __init__(self):
+        # criando a conexao com bd
+        self.connection = MySQLdb.connect(host='mysql.topskills.dev', database='topskills01', user='topskills01', passwd='ts2019' )
+        # definindo o cursor da conexao
+        self.cursor =  self.connection.cursor()
 
-class EstadoDao:#Criando a classe dao aonde ira conter os métodos
-    def __init__(self):#iniciando com  o construtar (__init__) ele é usado para inicializar o objeto para poder se criado a instancia
-        self.connection = MySQLdb.connect(host='mysql.topskills.dev', database='topskills01', user='topskills01', passwd='ts2019' )#criando a conexao com bd
-        self.cursor =  self.connection.cursor()#definindo o cursor da conexao
-
-    def listar_todos(self): # Criando método para listar todos da lista,do banco de dados
+    # Criando método para listar todos da lista,do banco de dados
+    def listar_todos(self):
         self.cursor.execute("SELECT * FROM topskills01.ESTADOS")
-        estados = self.cursor.fetchall() #o comando fetchall pega vários itens contidos na lista
-        lista_estados = []  #declarando lista vazia para receber os dados
-        for e in estados: #criando for e ,passando todos os valores contidos na variavél "estados" para a nova variavél ( e )
-            estado = EstadoModel(e[1], e[2], e[3], e[0])# criando uma variavél "estado" aonde recebe a classe "EstadoModel"
-            lista_estados.append(estado.__dict__) # adicionando na lista "lista_estados" através do metodo "append",
-            #os valores que estão contidos em "estados"
-        return lista_estados # retornando para  a lista "lista_estados"
+        # o comando fetchall pega vários itens contidos na lista
+        estados = self.cursor.fetchall()
+        # declarando lista vazia para receber os dados
+        lista_estados = []
+        # criando for e ,passando todos os valores contidos na variavél "estados" para a nova variavél ( e )
+        for e in estados:
+            # criando uma variavél "estado" aonde recebe a classe "EstadoModel"
+            estado = EstadoModel(e[1], e[2], e[3], e[0])
+            # adicionando na lista "lista_estados" através do metodo "append",os valores que estão contidos em "estados"
+            lista_estados.append(estado.__dict__) # adicionando na lista "lista_estados" através do metodo "append",os valores que estão contidos em "estados"
+        # retornando para  a lista "lista_estados"
+        return lista_estados
 
-    def buscar_por_id(self, id):#criando método "buscar_por_id", o parametro "self" faz a ligação, o id vem do arquivo estado_model
-        self.cursor.execute("SELECT * FROM topskills01.ESTADOS WHERE ID = {}".format(id))#passando o "self.cursor"para fazer a conexao" eo "execute"para executar o comando anterior
-        estado = self.cursor.fetchone()#criando uma variavél "estado" que equivale ao "self.cursor" que fera o fetchone=buscar um
-        e = EstadoModel(estado[1], estado[2], estado[3], estado[0])#criando a variavél "e" para poder receber,os valores do EstadoModel, e buscar eles por posição
-        return e.__dict__#um método do python que transfroma o valor em dicionário
+        # criando método "buscar_por_id", o parametro "self" faz a ligação, o id vem do arquivo estado_model
+    def buscar_por_id(self, id):
+        # passando o "self.cursor"para fazer a conexao" eo "execute"para executar o comando anterior
+        self.cursor.execute("SELECT * FROM topskills01.ESTADOS WHERE ID = {}".format(id))
+        # criando uma variavél "estado" que equivale ao "self.cursor" que fera o fetchone=buscar um
+        estado = self.cursor.fetchone()
+        # criando a variavél "e" para poder receber,os valores do EstadoModel, e buscar eles por posição
+        e = EstadoModel(estado[1], estado[2], estado[3], estado[0])
+        # um método do python que transfroma o valor em dicionário
+        return e.__dict__
 
-    def inserir(self):#criando método inserir.
+    # criando método inserir.
+    def inserir(self):
+        # pagando a varivél "estado"que contém os dados do bd, e adicioando ,através do comando INSERT... por parametro por ex:regiao populaca
         self.cursor.execute("""INSERT INTO topskills01.ESTADOS    
                             (regiao, estado, populacao)
-                            VALUES('{}', '{}', '{}')""".format(estado.regiao, estado.estado, estado.populacao))#pagando a varivél "estado"
-                            #que contém os dados do bd, e adicioando ,através do comando INSERT... por parametro por ex:regiao populacao
+                            VALUES('{}', '{}', '{}')""".format(estado.regiao, estado.estado, estado.populacao))
 
-    def atualizar(self, estado: EstadoModel):#Criando método para atualizar,passando o "EstadoModel" para trazer os parametros para se completar o código
+    # Criando método para atualizar,passando o "EstadoModel" para trazer os parametros para se completar o código
+    def atualizar(self, estado: EstadoModel):
         self.cursor.execute(""" UPDATE topskill01.ESTADOS 
                          SET
                              regiao
                              estado
                             populacao
                         WHERE ID = {}""".format(pessoa.nome, pessoa.sobrenome, pessoa.idade, pessoa.id))
-        self.connection.commit()#passnado a conexao e salvando a atualização através commit
-        return estado.__dict__ #passavdo os dados para dicionário através do "__dict__"
+        # passnado a conexao e salvando a atualização através commit
+        self.connection.commit()
+        # passavdo os dados para dicionário através do "__dict__"
+        return estado.__dict__
 
-    def delete(self):#criando méto delete para apagar
+        # criando méto delete para apagar
+    def delete(self):
         self.cursor.execute(""" DELETE FROM topskills01.ESTADOS WHERE ID = {}""".format(id) #passando comando do que e quero apagar
-
-
-                                )
+                     )
