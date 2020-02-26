077d122c3f80cbf86e58a5de569a3a1af4beb73a
GABRIEL <gabrielmal38@gmail.com>
2020-02-26 11:51:33 -0300

    aula61

---
+++ Aula61/convertendo.py
@@ -0,0 +1,12 @@
+a=int(input('Digite um número inteiro: '))
+a = bin(a)[2:]
+print(a)
+print((type(a)))
+a = str(a)
+print((type(a)))
+c = []
+#ab = a.strip('b')
+c,append(ab)
+
+print(c)
+for i in c:
